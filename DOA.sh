1b7e056e136d1e56bf87e26c0dd807d983f3b68d
Pranshu Ranakoti <r.pranshu@gmail.com>
2018-06-12 23:21:18 +0530

    Menu Driven UI
    Added a Menu driven UI

--- DOA.sh
+++ DOA.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 resize -s 17 69
 clear
+banner(){
 tput setaf 172;tput bold;echo $'            -= Built with <3 by PRANSHU RANAKOTI  ©2018 =-';tput sgr0;
 tput setaf 1;echo '
 ·▄▄▄▄  ▄▄▄ . ▄▄▄· ·▄▄▄▄            ▄▄▄       ▄▄▄· ▄▄▌  ▪   ▌ ▐·▄▄▄ .
@@ -8,42 +9,62 @@ tput setaf 1;echo '
 ▐█· ▐█▌▐▀▀▪▄▄█▀▀█ ▐█· ▐█▌     ▄█▀▄ ▐▀▀▄     ▄█▀▀█ ██▪  ▐█·▐█▐█•▐▀▀▪▄
 ██. ██ ▐█▄▄▌▐█ ▪▐▌██. ██     ▐█▌.▐▌▐█•█▌    ▐█ ▪▐▌▐█▌▐▌▐█▌ ███ ▐█▄▄▌
 ▀▀▀▀▀•  ▀▀▀  ▀  ▀ ▀▀▀▀▀•      ▀█▄▀▪.▀  ▀     ▀  ▀ .▀▀▀ ▀▀▀. ▀   ▀▀▀ '; tput sgr0
-
-echo $'\nA simple tool to check whether a particular host is Dead or Alive!'
-
-main ()
-{
-echo $'\nEnter\n 1 for Manual \n2 for reading from a file \n9 to reload the script\n0 to exit'
-read opt
+tput setaf 172
+echo $'\n A simple tool to check whether a particular host is Dead or Alive!\n';tput sgr0
 }
-main
+banner
+tput setaf 1;tput bold;echo -en '**************';
+tput setaf 214;tput bold;echo -en 'M';
+tput setaf 215;tput bold;echo -en 'E';
+tput setaf 216;tput bold;echo -en 'N';
+tput setaf 217;tput bold;echo -en 'U';
+tput setaf 1;echo '**************';tput setaf 1;tput bold;echo -en '*';
+tput setaf 2;  echo -en ' 1 Manual Mode                ';tput sgr0;
+tput setaf 1;tput bold;echo '*';tput sgr0;tput bold;tput setaf 1;echo -en '*';
+tput setaf 6;echo -en ' 2 Read Hosts from a file ';tput sgr0;
+tput setaf 1;tput bold;echo '    *';tput setaf 1;tput bold;echo -en '*';
+tput setaf 11;  echo -en ' 9 Reload                   ';tput sgr0;tput setaf 1;tput bold;echo '  *';
+tput setaf 1;echo -en '*';tput setaf 9; echo -en ' 0 Exit                     '
+tput sgr0;tput setaf 1;tput bold;echo '  *';
+tput bold;echo $'********************************';tput sgr0;
+read opt
 if [[ $opt -eq "1" ]]; then
+	clear
+	banner
+	tput setaf 172;tput bold;echo $'\n                              MANUAL MODE\n';tput sgr0
 	echo Enter the host DNS or IP address
 	read arg
 
 	ip=$(ping -c 1 $arg | grep -Eo '[0-9.]{9,}' | head -1)
 
 	if [[ $(ping -c 1 $arg) =~ "from" ]]; then
-		echo Congrats the host '('$ip')' is live!
-	else
-		echo host '('$ip')' is down
+		tput setaf 2; echo "[+] Congrats the host $arg ($ip) is live!"; tput sgr0
+	elif [[ $(ping -t 1 $arg) =~ "timeout" ]]; then
+		tput setaf 9; echo "[-] Bummer.... The host $arg ($ip) is down"; tput sgr0
 	fi
-	main
+	tput blink;tput setaf 1;tput bold;
+	echo "Press any key to continue";tput sgr0
+	read key
+	DOA
 elif [[ $opt -eq "2" ]]; then
-	echo 'Enter the path to the file which has the hosts (Line separated) with extension, eg: /Desktop/ip.txt.'
+	echo 'Enter the path to the file which has the hosts (Line separated) with extension, eg: /Desktop/ip.txt '
 	read path
+	tput setaf 1;echo 'This may take a while, so please be patient!';tput sgr0;
 	input=$path
-	while IFS= read -r var
+	while IFS= read -r arg
 	do
-	ip=$(ping -c 1 $var | grep -Eo '[0-9.]{9,}' | head -1)
+	ip=$(ping -c 1 $arg | grep -Eo '[0-9.]{9,}' | head -1)
 
-	if [[ $(ping -c 1 $var) =~ "from" ]]; then
-		tput setaf 2; echo Congrats the host $var '('$ip')' is live!; tput sgr0
-	else
-		tput setaf 1; echo host $var '('$ip')' is down; tput sgr0
-	fi 
+	if [[ $(ping -c 1 $arg) =~ "from" ]]; then
+		tput setaf 2; echo "[+] Congrats the host $arg ($ip) is live!"; tput sgr0
+	elif [[ $(ping -t 1 $arg;) =~ "timeout" ]]; then
+		tput setaf 9; echo "[-] Bummer.... The host $arg ($ip) is down"; tput sgr0
+	fi
 	done < "$input"
-	main 
+	tput blink;tput setaf 1;tput bold;
+	echo "Press any key to continue";tput sgr0
+	read key
+	DOA 
 elif [[ $opt -eq "9" ]]; then
 		DOA
 elif [[ $opt -eq "0" ]]; then
