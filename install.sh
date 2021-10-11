4339378d5e245561670bc92b6ed014ffe080fcbe
Pranshu Ranakoti <r.pranshu@gmail.com>
2021-10-11 13:13:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo mv DOA.sh /usr/local/bin/DOA;chmod +x /usr/local/bin/DOA
 echo "Press any key to start the tool";read a;DOA
 find . ! -name install.sh -delete
-echo "DOA has been added in your local bin, just type in DOA in your terminal to use the tool!" >> DOA.sh
+echo "DOA has been added in your local bin, just type in DOA in your terminal to use the tool!"
 sleep 0.5
 echo -en "Starting Dead or Alive"; sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;sleep 0.5 ;echo -en "." ;
 DOA
